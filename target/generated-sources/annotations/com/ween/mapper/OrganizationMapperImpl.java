27b7e1429150dfa446ae3135cbc8f7a369867036
umutcff <umidza-map101@code.edu.az>
2026-04-20 18:24:46 +0400

    push

--- target/generated-sources/annotations/com/ween/mapper/OrganizationMapperImpl.java
+++ target/generated-sources/annotations/com/ween/mapper/OrganizationMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-20T16:54:26+0400",
+    date = "2026-04-20T17:00:37+0400",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.18 (Microsoft)"
 )
 @Component
